f965dd8241c4a17e0af3eb30c6d339707c5d1295
Guido Bartolucci <guido@bartolucci.org>
2018-05-06 07:55:47 -0400

    Update knuth_dvi.py

--- experiments/knuth_dvi.py
+++ experiments/knuth_dvi.py
@@ -1,3 +1,7 @@
+# We will successively add layers of meaning to the stream of bytes. If we do our job well then
+# it will end up with one pass and the generators will be able to be collapsed into a simple
+# sequence of instructions.
+
 # 1. Convert stream of bytes into list of commands with operands
 operand_size = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
@@ -6,6 +10,6 @@ operand_size = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
                 1,2,3,4,5,2,3,4,5,16,17,18,19,15,29,10]
 commands = consume(open('foo.dvi'), operand_size)
-# Now we have an iterable of DVI commands, which are each a iterable of bytes.
+# Now we have an iterable of DVI commands, which are each an iterable of bytes.
 
 # 2. Add the state of the h, v, w, x, y, z variables at the end of each command
